da5fbdce082d0fc5b595b78d613adc651f6b99a1
Carlos Edgardo Medina Carranza <cmedinac@pe.ibm.com>
2018-01-23 18:03:00 -0500

    intento de efecto mostrar ocultar

--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
+
 
 @Component({
   selector: 'app-task',
@@ -10,6 +12,20 @@ export class TaskComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+
+    $("a").click(function (e) {
+      e.preventDefault();
+      $(this).find("i").toggle();
+      $(this).find("i").toggleClass("active");
+      $(this).parent().toggleClass("on");
+    });
+
+
+    $("#showHideButton").click(function () {
+      $("#alertDiv").toggleClass("fadedOut");
+    })
+
   }
 
 }
